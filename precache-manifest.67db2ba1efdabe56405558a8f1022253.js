b22bd139a74cbe7008ac7d1be3828b628e7c28d0
Denze1l <dvornikovd.o.1998@gmail.com>
2020-08-26 17:21:52 +0300

    Updates

---
+++ precache-manifest.67db2ba1efdabe56405558a8f1022253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8ad56edc44f2333ef591b8ee9b3ffe",
+    "url": "/video_app/index.html"
+  },
+  {
+    "revision": "d8d83184558919310cad",
+    "url": "/video_app/static/css/main.03123081.chunk.css"
+  },
+  {
+    "revision": "8f1e8df7dab961b9790a",
+    "url": "/video_app/static/js/2.f02b9e4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/video_app/static/js/2.f02b9e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d83184558919310cad",
+    "url": "/video_app/static/js/main.b30a4553.chunk.js"
+  },
+  {
+    "revision": "47467aef403563781f1f",
+    "url": "/video_app/static/js/runtime-main.9cfb5843.js"
+  }
+]);
